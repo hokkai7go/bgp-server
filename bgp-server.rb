e3a6c4cce64ecf90995fee8c919631c1cbec1202
Yutaro SUGAI <hokkai7go@gmail.com>
2025-12-18 07:18:13 +0000

    keepalive timer実装

--- bgp-server.rb
+++ bgp-server.rb
@@ -53,6 +53,10 @@ class BGPMessage
     ].pack('Cn n N C') + opt_params_payload
     new(TYPE_OPEN, payload)
   end
+
+  def self.build_keepalive
+    new(TYPE_KEEPALIVE, '')
+  end
 end
 
 class BGPMessageParser
@@ -92,12 +96,75 @@ class BGPSocket
 end
 
 class BGPSession
+  STATE_IDLE = :Idle
+  STATE_OPENSENT = :OpenSent
+  STATE_OPENCONFIRM = :OpenConfirm
+  STATE_ESTABLISHED = :Established
+
   def initialize(client_socket, my_as, my_router_id)
     @socket = client_socket
     @my_as = my_as
     @my_router_id = my_router_id
-    @state = :OpenSent
+    @state     = STATE_OPENSENT
+    @hold_time = 180
+    @keepalive_interval = 60
+    @hold_timer_thread  = nil
+    @keepalive_timer_thread = nil
     @parser = BGPMessageParser.new
+  end
+
+  def start_keepalive_timer
+    @keepalive_timer_thread = Thread.new do
+      loop do
+        sleep(@keepalive_interval)
+        send_keepalive
+      end
+    end
+  end
+
+  def send_keepalive
+    keepalive_message = BGPMessage.build_keepalive
+    @socket.write(keepalive_message.to_binary)
+    puts '--> Sent KEEPALIVE message.'
+  rescue => e
+    puts "Error sending KEEPALIVE: #{e.message}. Closing session."
+    terminate_session
+  end
+
+  def start_hold_timer
+    @hold_timer_thread.kill if @hold_timer_thread
+    @hold_timer_thread = Thread.new do
+      puts "Hold Timer started/reset to #{@hold_time}s."
+      begin
+        Timeout::timeout(@hold_time) do
+          Thread.stop
+        end
+      rescue Timeout::Error
+        puts '!!! Hold Timer expired. Terminating session.'
+        terminate_session('Hold Timer Expired')
+      end
+    end
+  end
+
+  def reset_hold_timer
+    # スレッドが実行中であれば、Timeout::timeout の待機状態を解除する
+    # Rubyのスレッド操作は複雑なので、ここでは単純にスレッドを再起動する簡易版を使う
+    start_hold_timer
+  end
+
+  def transition_to_established
+    @state = STATE_ESTABLISHED
+    start_keepalive_timer
+    start_hold_timer
+    puts '*** BGP Session is ESTABLISHED! ***'
+  end
+
+  def terminate_session(reason = 'Unknown Error')
+    puts "Terminating session. Reason: #{reason}"
+    @socket&.close
+    @keepalive_timer_thread&.kill
+    @hold_timer_thread&.kill
+    @state = STATE_IDLE
   end
 
   def send_open(hold_time = 180)
