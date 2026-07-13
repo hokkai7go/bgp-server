0dd41e1954ec4afd65761dfcdbc1ec7dd26b72c3
Yutaro Sugai <hokkai7go@gmail.com>
2026-07-13 22:21:04 +0900

    RIB実装

---
+++ lib/bgp_rib.rb
@@ -0,0 +1,26 @@
+class BGPRib
+  def initialize
+    @table = Hash.new { |hash, key| hash[key] = {} }
+  end
+
+  def add_route(prefix, peer_ip:, next_hop:, as_path:)
+    @table[prefix][peer_ip] = {
+      next_hop:    next_hop,
+      as_path:     as_path,
+      received_at: Time.now
+    }
+  end
+
+  def get_route(prefix, peer_ip)
+    @table[prefix][peer_ip]
+  end
+
+  # 特定のピアから受け取ったすべての経路を削除（セッション切断時用）
+  def remove_route_from_peer(peer_ip)
+    @table.each do |prefix, peers|
+      peers.delete(peer_ip)
+    end
+
+  @table.reject! { |_, peers| peers.empty? }
+  end
+end
